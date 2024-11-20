a0c6d3c36b0a117e62c117ce11db1a57fd14c359
benjakg <136550646+benjakg@users.noreply.github.com>
2024-11-20 09:42:36 -0400

    Initial commit

---
+++ estilos.css
@@ -0,0 +1,139 @@
+div h1
+{
+  color: #F5F5F5;
+  text-align: center;
+ border: 0px 0px 0px 0px;
+ background-color: rgb(93, 146, 146);
+  
+}
+*{
+    scroll-behavior: smooth;/*Permite que el desplazamiento sea suave*/
+    background-color: #F5F5F5;
+   color: rgb(64, 64, 164);
+}
+img{
+    width: 200px;
+}
+.title{
+    
+   
+    background-color: rgb(93, 146, 146);
+}
+.end{
+    background-color: rgb(93, 146, 146);
+}
+.footer
+{
+    border-top: 3px solid rgb(93, 146, 146);
+    background-color: rgb(90, 151, 151);
+    display: flex;
+    align-items: flex-end;
+}
+.footer p
+{
+   
+    color: rgb(90, 151, 151);
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.button {
+    transition: color 0.3s, opacity 0.3s;
+}
+
+/* Efecto hover */
+.button:hover {
+    color: grey;
+    opacity: 0.6;
+}
+
+
+ .ingresar
+ {
+    background-color: rgb(90, 151, 151);
+    border-color: whitesmoke;
+    color: aliceblue;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; 
+}
+ .ingresar:hover
+ {
+    color: grey;
+    opacity: 0.6;
+ }
+div h3,p
+{
+    color: rgb(69, 124, 124);
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+div p
+{
+   font-size:20px;
+   color: rgb(152, 188, 188);
+}
+.bar{
+    margin-bottom: 100px;
+}
+form{
+    border-top: 20px solid whitesmoke;
+    border-bottom: 60px solid whitesmoke;
+}
+.Descripcion{
+    border-bottom: 200px solid #F5F5F5;
+ 
+}
+.proyectos{
+    border-bottom: 400px solid #F5F5F5;
+ 
+}
+.tool
+{
+    border-top: 10px solid whitesmoke;
+    border-bottom: 90px solid whitesmoke;
+}
+header
+{
+ 
+    position: fixed; /* Fija el header en su lugar */
+    top: 0; /* Lo mantiene en la parte superior */
+    left: 0; /* Lo alinea a la izquierda */
+    width: 100%; /* Ocupa todo el ancho de la página */
+ 
+    color: white; /* Color del texto */
+    padding: 10px 20px; /* Espacio interno */
+    z-index: 1000; /* Asegura que el header esté encima de otros elementos */
+ 
+  
+   text-align: center;
+    background-color:rgb(90, 151, 151); /* Color de fondo */
+    color: white; /* Color del texto */
+
+ }
+nav{
+    position: fixed;/* Fija mi barra de navegacion en su lugar */
+   
+    
+   
+    
+  }
+  
+  .tool {
+    margin: 10px 0; /* Espacio entre los íconos */
+  }
+  .esp
+  {
+  height: 155px;
+  }
+  img{
+    max-width: 100%; 
+    height: auto;
+  }
+  .contacto
+  {
+    border-top: 130px solid #F5F5F5;
+  }
+  .proyectos{
+    border-top: 200px solid #F5F5F5;
+  }
+  .proyectos h2{
+    border-top: 200px solid #F5F5F5;
+  }
+ 
